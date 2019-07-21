469b006eb23b3ab88b49ce905f6c9016ac8c389b
Tom Mason <wheybags@wheybags.com>
2019-07-21 19:52:35 +0200

    Update SDL to v2.0.9

--- cmake/projects/SDL2/hunter.cmake
+++ cmake/projects/SDL2/hunter.cmake
@@ -131,6 +131,17 @@ hunter_add_version(
         PACKAGE_NAME
         SDL2
         VERSION
+        "2.0.9-p1"
+        URL
+        "https://github.com/hunter-packages/SDL2/archive/v2.0.9-p1.tar.gz"
+        SHA1
+        57487da2bca179eb63b90ef3a36f858dd339a424
+)
+
+hunter_add_version(
+        PACKAGE_NAME
+        SDL2
+        VERSION
         "2.0.4-urho-p2"
         URL
         "https://github.com/hunter-packages/SDL2/archive/v2.0.4-urho-p2.tar.gz"
